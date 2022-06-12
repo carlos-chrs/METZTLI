683de68af8fee4f3811ce6c3764b8f76a63ed7f6
CxrlosMX <LuisCRendon131@gmail.com>
2022-06-12 16:46:52 -0500

    Models update methods

--- aseembly/model.ts
+++ aseembly/model.ts
@@ -1,46 +1,56 @@
 import {PersistentVector, u128, context} from "near-sdk-as";
 
- @nearBindgen
+ 
 export class Product{
     // Atributtes
-    id: string;
-    name: string;
-    description: string;
-    image: string;
-    price: string;
-    owner:string;
-    public static fromPayload(payload: Product): Product {
-        const product = new Product(); //New Product
-        //Load the values to the product
-        product.id=payload.id;
-        product.name=payload.name;
-        product.description=payload.description;
-        product.image=payload.image;
-        product.price=payload.price;
-        product.owner = context.sender;
-        //return the product
-        return product;
+    name : String;
+    description : String
+    price : number;
+    owner : String;
+    // 
+    constructor(name : String, description : String, price : number){
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.owner = context.sender;
     }
 }
-@nearBindgen
+
+
+export class User{
+    name : String;
+    lastname : String;
+    email : String;
+    wallet : String;
+   
+    constructor(name : String,  lastname : String, email : String, wallet : String){
+        this.name = name;
+        this.lastname = lastname;
+        this.email = email;
+        this.wallet = wallet;
+        
+    }
+}
+
+
+
 export class Client { //Class Client
     //Attributtes
-    id:string;
-    user: string;
-    address: string;
+    name: String;
+    phonenumber: String;
+    address: String;
 
     // Method to load the values
-    public  static loadClient(loadClient: Client): Client{
-        let client=new Client();
-        client.id=loadClient.id;
-        client.user=loadClient.user;
-        client.address=loadClient.address;
-        //Return a Object Client
-        return client;
+    constructor(name : String,  phonenumber : String, address : String){
+        this.name = name;
+        this.phonenumber = phonenumber;
+        this.address = address;
+        
+        
     }
 }
 
 
-// export const productsStorage = new PersistentUnorderedMap<string, Product>("LISTED_PRODUCTS");
-
-// export const clientsStorage = new PersistentUnorderedMap<string, Client>("LISTED_CLIENTS");
+export const saveProduct = new PersistentVector<Product>("s");
+export const saveUser = new PersistentVector<User>("s");
+export const saveClient = new PersistentVector<Client>("s");
